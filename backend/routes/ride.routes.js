bafaa624580d24fdb338ef0ce3868c0abdcde389
nikhil-rathour <rathournikhil042@gmail.com>
2025-07-10 12:30:42 +0530

    ride route & controller added

---
+++ backend/routes/ride.routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { body } = require('express-validator');
+const rideController = require('../controllers/ride.contoller');
+const authMiddleware = require('../middelwares/auth.middelware');
+
+router.post("/create", 
+    authMiddleware.authUser,
+    body("pickup").isString().isLength({ min: 3 }).withMessage("pickupLocation is required"),
+    body("destination").isString().isLength({ min: 3 }).withMessage("destinationLocation is required"),
+    body("vehicleType").isString().isIn(["auto", "car", "moto"]).withMessage("vehicleType is required"),
+    rideController.createRide ,
+   
+
+) 
+
+
+
+module.exports = router;
