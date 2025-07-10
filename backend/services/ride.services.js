2a0024bb3f367c990b104e5548ed2c2040f3d685
nikhil-rathour <rathournikhil042@gmail.com>
2025-07-10 12:30:07 +0530

    ride service done

---
+++ backend/services/ride.services.js
@@ -0,0 +1,77 @@
+const rideModel = require('../models/ride.model');
+const mapsService = require('./maps.service');
+const crypto = require('crypto');
+
+
+async function getFare(pickup, destination) {
+
+    if (!pickup || !destination) {
+        throw new Error('Pickup and destination are required');
+    }
+
+    const distanceTime = await mapsService.getDistanceTime(pickup, destination);
+
+    const baseFare = {
+        auto: 25,
+        car: 45,
+        moto: 15
+    };
+
+    const perKmRate = {
+        auto: 8,
+        car: 10,
+        moto: 5
+    };
+
+    const perMinuteRate = {
+        auto: 2,
+        car: 3,
+        moto: 1.5
+    };
+
+
+
+    const fare = {
+        auto: Math.round(baseFare.auto + ((distanceTime.distance.value / 1000) * perKmRate.auto) + ((distanceTime.duration.value / 60) * perMinuteRate.auto)),
+        car: Math.round(baseFare.car + ((distanceTime.distance.value / 1000) * perKmRate.car) + ((distanceTime.duration.value / 60) * perMinuteRate.car)),
+        moto: Math.round(baseFare.moto + ((distanceTime.distance.value / 1000) * perKmRate.moto) + ((distanceTime.duration.value / 60) * perMinuteRate.moto))
+    };
+ 
+    return fare;
+
+
+}
+
+module.exports.getFare = getFare;
+
+
+
+function getOtp(num) {
+    function generateOtp(num) {
+        const otp = crypto.randomInt(Math.pow(10, num - 1), Math.pow(10, num)).toString();
+        return otp;
+    }
+    return generateOtp(num);
+}
+
+module.exports.createRide = async ({
+    user, pickup, destination, vehicleType
+}) => {
+    if (!user || !pickup || !destination || !vehicleType) {
+        throw new Error('All fields are required');
+    }
+
+    const fare = await getFare(pickup, destination);
+
+
+
+    const ride = rideModel.create({
+        user,
+        pickup,
+        destination,
+        otp: getOtp(6),
+        fare: fare[ vehicleType ]
+    })
+
+    return ride;
+}
