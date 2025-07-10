f140289462a7996912195dd677d75bc9e589c29b
nikhil-rathour <rathournikhil042@gmail.com>
2025-07-10 12:33:22 +0530

    consume  routes

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,11 @@ const app = express()
 const cors = require("cors");
 const connectToDB = require("./db/db");
 const cookieParser = require("cookie-parser")
+const mapsRoutes = require("./routes/maps.route")
 
 const userRoutes = require("./routes/user.routes")
 const captainRoutes = require("./routes/captain.route")
+const rideRoutes = require("./routes/ride.routes")
 
 connectToDB()
 
@@ -19,6 +21,8 @@ app.use(express.json())
 app.use("/users", userRoutes)
 app.use(express.urlencoded({extended: true}))
 app.use("/captains", captainRoutes)
+app.use("/maps", mapsRoutes)
+app.use("/rides", rideRoutes)
 
 app.get("/" , (req,res)=>{
     res.send("heloooo")
